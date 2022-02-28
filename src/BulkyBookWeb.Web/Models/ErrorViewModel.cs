ba1aa2f7a319ae55d4c1756711a260601ec71401
bmassioui <bouchaib.massioui@gmail.com>
2022-02-28 17:23:44 +0000

    initialize the project

---
+++ src/BulkyBookWeb.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyBookWeb.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
